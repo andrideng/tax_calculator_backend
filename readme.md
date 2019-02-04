422badfbf71947a0fa557ce8e858e6c17dc40528
Andri Deng <andri.deng@bvolo.com>
2019-02-04 17:50:02 +0700

    add dockerfile

--- readme.md
+++ readme.md
@@ -2,7 +2,13 @@
 
 ## Features
 
-...
+* Get List of bills complete with the total price, total tax price and grand total.
+* Add New Bill.
+
+## Requirement
+
+* Golang v1.5 or above.
+* Mysql v5.7 or above
 
 ## Getting Started
 
@@ -12,7 +18,7 @@ install Go on your computer. Requires Go 1.5 or above.
 After installing Go, run the following commands to download and install this project:
 
 ```shell
-# install the proejct
+# install the project
 go get github.com/andrideng/tax-calculator
 
 # install dep
@@ -33,8 +39,10 @@ The application runs as an HTTP server at port 8080. It provides the following R
 
 * `GET /api/`: welcoming text api
 * `GET /api/ping`: a ping service mainly provided for health check purpose
+* `GET /api/bills`: list all bills with the price total, tax total and grand total.
+* `POST /api/bills`: create a bill.
 
-API Documentation [...]
+API Documentation [https://documenter.getpostman.com/view/528724/RztmsovD]
 
 
 For example, if you access the URL `http://localhost:8080/api/ping` in a browser, you should see the browser
--- testdata/db.sql
+++ testdata/db.sql
@@ -1,7 +1,59 @@
--- create database
-CREATE DATABASE tax_calculator_db;
+-- # ************************************************************
+-- # Sequel Pro SQL dump
+-- # Version 4541
+-- #
+-- # http://www.sequelpro.com/
+-- # https://github.com/sequelpro/sequelpro
+-- #
+-- # Host: 127.0.0.1 (MySQL 5.7.20)
+-- # Database: tax_calculator_db
+-- # Generation Time: 2019-02-04 10:06:49 +0000
+-- # ************************************************************
 
--- Tax Table
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- # Dump of table bills
+-- # ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `bills`;
+
+CREATE TABLE `bills` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `tax_code` int(11) DEFAULT NULL,
+  `price` float DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+LOCK TABLES `bills` WRITE;
+/*!40000 ALTER TABLE `bills` DISABLE KEYS */;
+
+INSERT INTO `bills` (`id`, `name`, `tax_code`, `price`, `created_at`, `updated_at`)
+VALUES
+	(1,'Lucky Stretch',2,1000,'2019-02-03 13:23:15','2019-02-03 13:23:15'),
+	(2,'Big Mac',1,1000,'2019-02-03 13:23:34','2019-02-03 13:23:34'),
+	(3,'Movie',3,150,'2019-02-03 13:23:51','2019-02-03 13:23:51'),
+	(4,'Avenger',3,200,'2019-02-03 15:36:12','2019-02-03 15:36:12');
+
+/*!40000 ALTER TABLE `bills` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+-- # Dump of table taxs
+-- # ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `taxs`;
+
 CREATE TABLE `taxs` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `tax_code` int(11) DEFAULT NULL,
@@ -12,20 +64,23 @@ CREATE TABLE `taxs` (
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- INSERT TAXS QUERY
-INSERT into taxs(`tax_code`, `type`, `formula`, `refundable`, `created_at`, `updated_at`) VALUES(1, 'Food & Beverage', '0.01 * {price}', 'YES', now(), now());
-INSERT into taxs(`tax_code`, `type`, `formula`, `refundable`, `created_at`, `updated_at`) VALUES(2, 'Tobacco', '10 + (0.02 * {price})', 'NO', now(), now());
-INSERT into taxs(`tax_code`, `type`, `formula`, `condition`, `refundable`, `created_at`, `updated_at`) VALUES(3, 'Entertainment', '0.01 * ({price} - 100)', '0 < {price} < 100', 'NO', now(), now());
+LOCK TABLES `taxs` WRITE;
+/*!40000 ALTER TABLE `taxs` DISABLE KEYS */;
 
--- Bill Table
-CREATE TABLE `bills` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) DEFAULT NULL,
-  `tax_code` int(11) DEFAULT NULL,
-  `price` float DEFAULT NULL,
-  `created_at` datetime DEFAULT NULL,
-  `updated_at` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+INSERT INTO `taxs` (`id`, `tax_code`, `type`, `condition`, `formula`, `refundable`, `created_at`, `updated_at`)
+VALUES
+	(1,1,'Food & Beverage','','0.1 * {price}','YES','2019-02-02 01:30:49','2019-02-02 01:30:49'),
+	(2,2,'Tobacco','','10 + (0.02 * {price})','NO','2019-02-02 01:30:49','2019-02-02 01:30:49'),
+	(3,3,'Entertainment','{price} >= 100','0.01 * ({price} - 100)','NO','2019-02-02 01:32:16','2019-02-02 01:32:16');
+
+/*!40000 ALTER TABLE `taxs` ENABLE KEYS */;
+UNLOCK TABLES;
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
